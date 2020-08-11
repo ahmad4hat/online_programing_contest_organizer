2125ffdd47e3965d77317bf9846734a4264552e0
Ahmad Farhat <rappar.stand@gmail.com>
2020-08-12 02:34:50 +0600

    profile added and functional , edit profile ui added

--- header.php
+++ header.php
@@ -3,6 +3,8 @@
         <ul>
             <li><a href="home.php">Home</a></li>
             <li><a href="profile.php">Profile</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="registration.php">Registration</a></li>
         </ul>
     </nav>
 </header>
