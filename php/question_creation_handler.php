09a278d4d03f68e954181334e01ca6d4d9168f6a
Ahmad Farhat <rappar.stand@gmail.com>
2020-09-22 22:45:36 +0600

    question creation javascript validation doen

--- php/question_creation_handler.php
+++ php/question_creation_handler.php
@@ -1,5 +1,10 @@
 <?php include '../view/partials/helper.php'; ?>
 <?php
+if($user){
+    $returnValue=json_decode($_POST["json"],true);
+    print_r($returnValue);
 
-$returnValue=json_decode($_POST["json"],true);
-print_r($user);
+
+}
+   
+
--- view/question_creation.php
+++ view/question_creation.php
@@ -11,17 +11,20 @@
     <form action="" method="post" id="questionForm">
 
         <label for="problemStatement">Problem statement :</label>
+        <p class="errorText hidden"  id="problemStatementErrorText">Hello </p>
         <br>
         <textarea name="problemStatement" id="problemStatement" cols="50" rows="10"></textarea>
         <br>
         <br>
         <label for="expectedOutput">Expected Output :</label>
+        <p class="errorText hidden"  id="expectedOutputErrorText">Hello </p>
         <br>
         <textarea name="expectedOutput" id="expectedOutput" cols="50" rows="10"></textarea>
         <br>
         <br>
 
         <label for="totalMark">Total Mark :</label>
+        <p class="errorText hidden"  id="totalMarkErrorText">Hello </p>
         <input name="totalMark" type="number" id="totalMark"></input>
         <br>
         <br>
@@ -32,7 +35,7 @@
 
         <label for="difficulty">Difficulty :</label>
         <select name="difficulty" id="difficulty" required>
-            <option value="easy">Easy</option>
+            <option default value="easy">Easy</option>
             <option value="medium">Medium</option>
             <option value="hard">Hard</option>
 
@@ -41,6 +44,7 @@
         <label for="lastSubmissionDate">
         Last Submission Date
         </label>
+        <p class="errorText hidden"  id="lastSubmissionDateErrorText">Hello </p>
         <input type="date" name="lastSubmissionDate" id="lastSubmissionDate">
 
         <br>
@@ -77,15 +81,144 @@
     };
     //   return value;
     };
+    </script>
     
+    <script>
+
+
     const formEl = document.querySelector("#questionForm");
+    
     const problemStatementEl= document.querySelector('#problemStatement');
     const expectedOutputEl= document.querySelector('#expectedOutput');
     const totalMarkEl= document.querySelector('#totalMark');
     const difficultyEl= document.querySelector('#difficulty');
     const lastSubmissionDateEl= document.querySelector('#lastSubmissionDate');
+
+    const problemStatementErrorTextEl= document.querySelector('#problemStatementErrorText');
+    const expectedOutputErrorTextEl= document.querySelector('#expectedOutputErrorText');
+    const totalMarkErrorTextEl= document.querySelector('#totalMarkErrorText');
+    // const difficultyEl= document.querySelector('#difficulty');
+    const lastSubmissionDateErrorTextEl= document.querySelector('#lastSubmissionDateErrorText');
+
+    const validateProblemStatement=(value)=>{
+        let errorText =null ;
+        if(!value){
+            errorText="Problem Statement can not be empty";
+        }else if(value==""){
+            errorText="Problem Statement can not be empty";
+        }
+        else if (value.length < 20){
+            errorText="Problem Statement at least 20 character long "
+        }
+        if(errorText){
+            problemStatementErrorTextEl.innerText= errorText;
+            problemStatementErrorTextEl.style.display="inline"
+
+        }else{
+            problemStatementErrorTextEl.innerText= null;
+            problemStatementErrorTextEl.style.display="none"
+
+        }
+
+        return errorText
+    }
+
+    problemStatementEl.addEventListener('keyup',(event)=> validateProblemStatement(event.target.value));
+   
+
+    const validateExpectedOutput=(value)=>{
+        let errorText =null ;
+        
+        if(!value){
+            errorText="Expected Output can not be empty";
+        }else if(value==""){
+            errorText="Expected Output can not be empty";
+        }
+        else if (value.length < 5){
+            errorText="Expected Output at least 5 character long "
+        }
+        if(errorText){
+            expectedOutputErrorTextEl.innerText= errorText;
+            expectedOutputErrorTextEl.style.display="inline"
+
+        }else{
+            expectedOutputErrorTextEl.innerText= null;
+            expectedOutputErrorTextEl.style.display="none"
+
+        }
+
+        return errorText
+    }
+
+    expectedOutputEl.addEventListener('keyup',(event)=> validateExpectedOutput(event.target.value))
+
     
+    const validateTotalMark=(value)=>{
+        let errorText =null ;
+        
+        if(!value){
+            errorText="Total Mark can not be empty";
+        }else if(value==""){
+            errorText="Total Mark  can not be empty";
+        }
+        else if (isNaN(value)){
+            errorText="Total number must ber a number";
+        } else {
+            valueInt = parseInt(value)
+            if (valueInt < 0 || valueInt >500 ){
+
+                errorText="Total must be between 0 and 500";
+                console.log(errorText);
+
+            }
+        }
+        
+        if(errorText){
+            totalMarkErrorTextEl.innerText= errorText;
+            totalMarkErrorTextEl.style.display="inline"
+
+        }else{
+            totalMarkErrorTextEl.innerText= null;
+            totalMarkErrorTextEl.style.display="none"
+
+        }
+
+        return errorText
+    }
+
+    totalMarkEl.addEventListener('keyup',(event)=> validateTotalMark(event.target.value))
+
+
+    const validateLastSubmissionDate=(value)=>{
+        let errorText =null ;
+
+        const date = new Date(value);
+
+        
+        if(!value){
+            errorText="Submission Date can not be empty";
+        }else if(value==""){
+            errorText=" Submission Date can not be empty";
+        }else if (date< new Date()){
+            errorText="Last submission date must be after today " ;
+
+        }
     
+        
+        if(errorText){
+            lastSubmissionDateErrorTextEl.innerText= errorText;
+            lastSubmissionDateErrorTextEl.style.display="inline"
+
+        }else{
+            lastSubmissionDateErrorTextEl.innerText= null;
+            lastSubmissionDateErrorTextEl.style.display="none"
+
+        }
+
+        return errorText
+    }
+    lastSubmissionDateEl.addEventListener('change',(event)=> validateLastSubmissionDate(event.target.value))
+
     formEl.addEventListener("submit",(event)=>{
         event.preventDefault();
         let problemStatement = problemStatementEl.value;
@@ -93,8 +226,16 @@
         let totalMark = totalMarkEl.value;
         let difficulty = difficultyEl.value;
         let lastSubmissionDate = lastSubmissionDateEl.value;
-        
-        
+
+
+        if(validateProblemStatement(problemStatement) ||
+        validateExpectedOutput(expectedOutput) || 
+        validateTotalMark(totalMark)|| 
+        validateLastSubmissionDate(lastSubmissionDate)){
+            return false ;
+
+        }
+
         const sendObject= {
             problemStatement,
             expectedOutput,
@@ -111,11 +252,12 @@
 
         // console.log(sendObject);
          const sendJson= JSON.stringify(sendObject);
+
         ajaxJson('../php/question_creation_handler.php',sendJson,(v)=>console.log(v));
 
-        
+
     })
-    
+
 
     </script>
 
